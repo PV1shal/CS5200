295b3f7c6b4dcce5cade07121dfa163373d8060a
Alex Tsao <alexandertsao@Alexs-Windows-PC.local>
2023-11-06 19:03:14 -0800

    Host version one - not tested

---
+++ AirbnbApplication/src/main/java/airbnb/dao/HostsDao.java
@@ -0,0 +1,203 @@
+package airbnb.dao;
+
+import airbnb.model.*;
+import airbnb.dao.ConnectionManager;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * HostsDao is a singleton class responsible for managing database operations for Host entities.
+ */
+public class HostsDao{
+	
+	
+	protected ConnectionManager connectionManager;
+	
+	// Single pattern: instantiation is limited to one object.
+	private static HostsDao instance = null;
+	protected HostsDao() {
+		connectionManager = new ConnectionManager();
+	}
+	public static HostsDao getInstance() {
+		if(instance == null) {
+			instance = new HostsDao();
+		}
+		return instance;
+	}
+	
+	public Hosts create(Hosts host) throws SQLException {
+		String insertHost = "INSERT INTO Hosts(HostId,HostUrl,HostName, HostSince, HostResponseTime, HostResponseRate, HostTotalListingCount, HostVerification) "
+				+ "  VALUES(?,?,?,?,?,?,?,?);";
+		Connection connection = null;
+		PreparedStatement insertStmt = null;
+		try {
+		
+			connection = connectionManager.getConnection();
+			insertStmt = connection.prepareStatement(insertHost);
+		
+			
+			insertStmt.setInt(1, host.getHostId());
+			insertStmt.setString(2, host.getHostUrl());
+			insertStmt.setString(3, host.getHostName());
+			insertStmt.setTimestamp(4, new Timestamp(host.getHostSince().getTime()));
+			insertStmt.setString(5, host.getHostResponseTime());
+			insertStmt.setInt(6, host.getHostResponseRate());
+			insertStmt.setInt(7, host.getHostTotalListingCount());
+			insertStmt.setString(8, host.getHostVerification());
+				
+
+			insertStmt.executeUpdate();
+		
+			return host;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(insertStmt != null) {
+				insertStmt.close();
+			}
+		}
+	}
+	
+	
+	public Hosts getHostsByHostId(int hostId)throws SQLException {
+		String selectHost = "SELECT HostId, HostUrl, HostName, HostSince, HostResponseTime, HostResponseRate, HostTotalListingCount, HostVerification"
+				+ " FROM Hosts "
+				+ "WHERE HostId=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectHost);
+			selectStmt.setInt(1, hostId);
+		
+			
+			results = selectStmt.executeQuery();
+		
+			
+			if(results.next()) {
+				int retrievedHostId = results.getInt("HostId");
+		        String hostUrl = results.getString("HostUrl");
+		        String hostName = results.getString("HostName");
+		        Date hostSince =  new Date(results.getTimestamp("HostSince").getTime());
+		        String hostResponseTime = results.getString("HostResponseTime");
+		        int hostResponseRate = results.getInt("HostResponseRate");
+		        int hostTotalListingCount = results.getInt("HostTotalListingCount");
+		        String hostVerification = results.getString("HostVerification");
+
+		        Hosts host = new Hosts(retrievedHostId, hostUrl, hostName, hostSince, hostResponseTime, hostResponseRate, hostTotalListingCount, hostVerification);
+		        
+				return host;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return null;
+	}
+	
+	
+	public List<Hosts> getHostsByHostName(String hostName)throws SQLException {
+	
+		List<Hosts> hosts = new ArrayList<Hosts>();
+		
+		String selectHost = "SELECT HostId, HostUrl, HostName, HostSince, HostResponseTime, HostResponseRate, HostTotalListingCount, HostVerification"
+				+ " FROM Hosts "
+				+ "WHERE HostName= ?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectHost);
+			selectStmt.setString(1, hostName);
+		
+			
+			results = selectStmt.executeQuery();
+		
+			
+			while(results.next()) {
+				int retrievedHostId = results.getInt("HostId");
+		        String hostUrl = results.getString("HostUrl");
+		       
+		        Date hostSince =  new Date(results.getTimestamp("HostSince").getTime());
+		        String hostResponseTime = results.getString("HostResponseTime");
+		        int hostResponseRate = results.getInt("HostResponseRate");
+		        int hostTotalListingCount = results.getInt("HostTotalListingCount");
+		        String hostVerification = results.getString("HostVerification");
+
+		        Hosts host = new Hosts(retrievedHostId, hostUrl, hostName, hostSince, hostResponseTime, hostResponseRate, hostTotalListingCount, hostVerification);
+		        
+				hosts.add(host);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return hosts;
+	}
+	
+	
+	public Hosts delete(Hosts host) throws SQLException {
+		
+		String deleteHost = "DELETE FROM Hosts WHERE HostId=?;";
+		Connection connection = null;
+		PreparedStatement deleteStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			deleteStmt = connection.prepareStatement(deleteHost);
+			deleteStmt.setInt(1, host.getHostId());
+			deleteStmt.executeUpdate();
+
+			
+			
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(deleteStmt != null) {
+				deleteStmt.close();
+			}
+		}
+		
+	}
+	
+}
--- AirbnbApplication/src/main/java/airbnb/model/Hosts.java
+++ AirbnbApplication/src/main/java/airbnb/model/Hosts.java
@@ -1,5 +1,88 @@
 package airbnb.model;
 
+import java.util.Date;
+
 public class Hosts {
+	protected int hostId;
+	protected String hostUrl;
+	protected String hostName;
+	protected Date hostSince;
+	protected String hostResponseTime;
+	protected int hostResponseRate;
+	protected int hostTotalListingCount;
+	protected String hostVerification;
+	
+	
+	public Hosts(int hostId, String hostUrl, String hostName, Date hostSince, String hostResponseTime,
+			int hostResponseRate, int hostTotalListingCount, String hostVerification) {
+		this.hostId = hostId;
+		this.hostUrl = hostUrl;
+		this.hostName = hostName;
+		this.hostSince = hostSince;
+		this.hostResponseTime = hostResponseTime;
+		this.hostResponseRate = hostResponseRate;
+		this.hostTotalListingCount = hostTotalListingCount;
+		this.hostVerification = hostVerification;
+	}
+	
+	
+	public Hosts(int hostId) {
+		this.hostId = hostId;
+	}
+	
+	
+	
+	public int getHostId() {
+		return hostId;
+	}
+	public void setHostId(int hostId) {
+		this.hostId = hostId;
+	}
+	public String getHostUrl() {
+		return hostUrl;
+	}
+	public void setHostUrl(String hostUrl) {
+		this.hostUrl = hostUrl;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	public Date getHostSince() {
+		return hostSince;
+	}
+	public void setHostSince(Date hostSince) {
+		this.hostSince = hostSince;
+	}
+	public String getHostResponseTime() {
+		return hostResponseTime;
+	}
+	public void setHostResponseTime(String hostResponseTime) {
+		this.hostResponseTime = hostResponseTime;
+	}
+	public int getHostResponseRate() {
+		return hostResponseRate;
+	}
+	public void setHostResponseRate(int hostResponseRate) {
+		this.hostResponseRate = hostResponseRate;
+	}
+	public int getHostTotalListingCount() {
+		return hostTotalListingCount;
+	}
+	public void setHostTotalListingCount(int hostTotalListingCount) {
+		this.hostTotalListingCount = hostTotalListingCount;
+	}
+	public String getHostVerification() {
+		return hostVerification;
+	}
+	public void setHostVerification(String hostVerification) {
+		this.hostVerification = hostVerification;
+	}
+	
+	
+	
+	
 
 }
